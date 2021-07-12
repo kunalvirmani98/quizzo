03bc8a0f03264db5a6dd91914a6a8dc2c0d0c442
Kunal Virmani <kunalvirmani@Kunals-MacBook-Pro.local>
2021-07-12 22:41:04 +0530

    added lifecycle method to inputComponent

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
     <div className="m-2">
       <h1 className="display-1 text-center bg-info py-4">Quizzo</h1>
       <h2>Q. What’s your favorite way to spend a day off?</h2>
-      <InputComponent name="inputComponent1" id="id1" isChecked="false" optionVal="Gaming" />
-      <InputComponent name="inputComponent2" id="id2" isChecked="true" optionVal="Netflix and chill" />
-      <InputComponent name="inputComponent3" id="id3" isChecked="false" optionVal="Travelling" />
-      <InputComponent name="inputComponent4" id="id4" isChecked="false" optionVal="Reading" />
+      <InputComponent name="inputComponent1" id="id1" optionVal="Gaming" />
+      <InputComponent name="inputComponent2" id="id2" optionVal="Netflix and chill" />
+      <InputComponent name="inputComponent3" id="id3" optionVal="Travelling" />
+      <InputComponent name="inputComponent4" id="id4" optionVal="Reading" />
     </div>
   );
 }
--- src/inputComponent.js
+++ src/inputComponent.js
@@ -6,18 +6,27 @@ export default class InputComponent extends React.Component {
         this.state = {
             name: this.props.name,
             id: this.props.id,
-            isChecked: this.props.isChecked,
+            isChecked: false,
             optionVal: this.props.optionVal
         };
+        this.handleToggle = this.handleToggle.bind(this);
     }
+
+    handleToggle(e) {
+        e.preventDefault();
+        if (this.state.isChecked)
+            this.setState({ isChecked: false })
+        else
+            this.setState({ isChecked: true })
+        console.log(this.state.isChecked);
+    }
+
     render() {
-        const isChecked = this.props.isChecked === "false" ? false : true;
         return (
             <div class="form-check">
-                {isChecked ?
-                    <input class="form-check-input" type="radio" name={this.state.name} id={this.state.id} value={this.props.isChecked} checked /> :
-                    <input class="form-check-input" type="radio" name={this.state.name} id={this.state.id} value={this.props.isChecked} />}
-                <label class="form-check-label" for="{this.state.name}">
+                <input className="form-check-input" type="radio" onChange={this.handleToggle} name={this.state.name} id={this.state.id} defaultChecked={this.state.isChecked} />
+
+                <label className="form-check-label" htmlFor="{this.state.name}">
                     {this.props.optionVal}
                 </label>
             </div>
