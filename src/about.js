bfa2b40ae18f1e31563c799a0fd5bcda92b81e45
Kunal Virmani <kunalvirmani@Kunals-MacBook-Pro.local>
2021-08-24 18:13:41 +0530

    made changes to about page

--- src/about.js
+++ src/about.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import devLogo from './cartoon-dev.jpg';
 
 class About extends React.Component {
-
 
     componentDidMount() {
         this.props.changeActivePage('about');
     }
 
     render() {
-        return (<h1>About</h1>)
+        return (
+            <div className="body-container">
+                <div className="d-flex justify-content-center">
+                    <div className="d-flex flex-column justify-content-center m-5">
+                        <h1 className="display-1"><strong>Hi, I'm Kunal</strong></h1>
+                        <h5 className="text-secondary">A passionate Software Developer having an experience of building web applications
+                            using React / JavaScript / Nodejs and some other cool libraries and frameworks. I am currenly working in ______ as a
+                            Software Developer and open to new opportunities
+                        </h5>
+                    </div>
+                    <img src={devLogo} className="box-shadow-custom" style={{ width: "550px", height: "550px", borderRadius: "50%" }} alt="logo" />
+                </div>
+            </div>
+        )
     }
 }
 
