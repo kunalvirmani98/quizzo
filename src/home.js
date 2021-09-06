0e44f6e77a1bd682ec0cff0a241b274050862158
Kunal Virmani <kunalvirmani@Kunals-MacBook-Pro.local>
2021-09-06 23:07:02 +0530

    made more changes to home page

--- src/home.js
+++ src/home.js
@@ -11,14 +11,19 @@ class Home extends React.Component {
         return (
             <div className="body-container">
                 <div className="d-flex justify-content-center">
-                    <img src={logo} style={{ width: "450px", height: "500px" }} alt="logo" />
+                    <img src={logo} style={{ width: "450px", height: "500px", marginTop: "5rem" }} alt="logo" />
                     <div className="m-5">
                         <h1 className="display-3"><strong>Where quiz meets entertainment!</strong></h1>
                         <h4>Unlocking knowledge at the speed of thought.</h4>
                         <br />
+                        <h5>Quizzo is a refreshing quiz game that makes learning easy ,motivates you to learn more and serves the purpose of
+                            testing your learning
+                        </h5>
+                        <button className="btn btn-primary">Learn more</button>
+                        <br />
                         <br />
                         <h5>Feeling bored? Why not take a quiz</h5>
-                        <button class="btn btn-primary submit-btn flex-end" type="submit">Start quiz</button>
+                        <button className="btn btn-primary submit-btn flex-end" type="submit">Start quiz</button>
                     </div>
                 </div>
                 <div></div>
